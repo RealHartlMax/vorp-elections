a2a6da80bdeac8c546a7341e1be3ff4525f22a1e
RealHartlMax <121199770+RealHartlMax@users.noreply.github.com>
2025-06-30 07:19:00 +0200

    Aktualisieren von fxmanifest.lua
    change loading order, first config.lua

--- democracy/fxmanifest.lua
+++ democracy/fxmanifest.lua
@@ -4,14 +4,14 @@ game "rdr3"
 author 'Jeffy & Beffy of Godz Country'
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
-client_scripts {
-	'client.lua',
-	
-}
-
 shared_scripts {
 	'config.lua',
 
+}
+
+client_scripts {
+	'client.lua',
+	
 }
 
 server_scripts {
